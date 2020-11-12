c8a863dc6b39a7b7f99b648eac8962815eec4a28
Fauzan <ojinsyo@gmail.com>
2020-11-12 09:00:28 +0700

    publish

--- src/utils/global-variable.js
+++ src/utils/global-variable.js
@@ -1,14 +1,15 @@
 export const globalVariable = (request) => {
-    switch (request) {
-        case "frontendAddress":
-            return "http://localhost:3001";
-        //return "http://34.101.65.216:3000"
-        //return "http://34.101.93.85:3000"
-        //return "https://sibandar-react.et.r.appspot.com"
-        //return "http://192.168.100.78:3000"
-        case "backendAddress":
-            //return "http://35.186.157.80:8080"
-            //return "http://192.168.100.78:8080"
-            return "http://localhost:3000";
-    }
+  switch (request) {
+    case "frontendAddress":
+      return "http://localhost:3000";
+    //return "http://34.101.65.216:3000"
+    //return "http://34.101.93.85:3000"
+    //return "https://sibandar-react.et.r.appspot.com"
+    //return "http://192.168.100.78:3000"
+    case "backendAddress":
+      //return "http://35.186.157.80:8080"
+      //return "http://192.168.100.78:8080"
+      //return "http://localhost:8080";
+      return "http://13.250.44.215:8080";
+  }
 };
