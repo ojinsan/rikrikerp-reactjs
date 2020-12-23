a8d6e60a5bf303b8953eb5752302f15413963849
Fauzan <ojinsyo@gmail.com>
2020-12-23 08:49:39 +0700

    deleting some navigation

--- src/elements/Sidebar/Sidebar.js
+++ src/elements/Sidebar/Sidebar.js
@@ -2,11 +2,11 @@ import React from "react";
 
 import { Layout, Menu, Breadcrumb } from "antd";
 import {
-    DesktopOutlined,
-    PieChartOutlined,
-    FileOutlined,
-    TeamOutlined,
-    UserOutlined,
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
 } from "@ant-design/icons";
 
 import { Link, useLocation } from "react-router-dom";
@@ -15,35 +15,35 @@ const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 const Sidebar = (props) => {
-    const [collapsed, changeCollapsed] = React.useState(false);
-    const onCollapse = (collapsed) => {
-        changeCollapsed(collapsed);
-    };
-    const location = useLocation();
-    console.log("Sidebar");
-    console.log(location.pathname);
-    return (
-        <>
-            <Sider
-                collapsible
-                collapsed={collapsed}
-                onCollapse={onCollapse}
-                style={{ backgroundColor: "white" }}
-            >
-                <div className="logo" />
-                <Menu
-                    theme="white"
-                    defaultSelectedKeys={[location.pathname]}
-                    mode="inline"
-                >
-                    <Menu.Item key="/dashboard" icon={<PieChartOutlined />}>
-                        <Link to="/dashboard">Dashboard</Link>
-                    </Menu.Item>
-                    <SubMenu key="sub1" icon={<UserOutlined />} title="Project">
-                        <Menu.Item key="/project">
-                            <Link to="/project">Project</Link>
-                        </Menu.Item>
-                        <Menu.Item key="/project/rab-project-bagian">
+  const [collapsed, changeCollapsed] = React.useState(false);
+  const onCollapse = (collapsed) => {
+    changeCollapsed(collapsed);
+  };
+  const location = useLocation();
+  console.log("Sidebar");
+  console.log(location.pathname);
+  return (
+    <>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={onCollapse}
+        style={{ backgroundColor: "white" }}
+      >
+        <div className="logo" />
+        <Menu
+          theme="white"
+          defaultSelectedKeys={[location.pathname]}
+          mode="inline"
+        >
+          <Menu.Item key="/dashboard" icon={<PieChartOutlined />}>
+            <Link to="/dashboard">Dashboard</Link>
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="Project">
+            <Menu.Item key="/project">
+              <Link to="/project">Project</Link>
+            </Menu.Item>
+            {/* <Menu.Item key="/project/rab-project-bagian">
                             <Link to="/project/rab-project-bagian">
                                 RAB Project Bagian
                             </Link>
@@ -53,31 +53,27 @@ const Sidebar = (props) => {
                         </Menu.Item>
                         <Menu.Item key="/project/ahs-project">
                             <Link to="/project/ahs-project">AHS Project</Link>
-                        </Menu.Item>
-                    </SubMenu>
-                    <SubMenu
-                        key="sub2"
-                        icon={<TeamOutlined />}
-                        title="Data Source"
-                    >
-                        <Menu.Item
-                            key="/ahs-sumber"
-                            // icon={<PieChartOutlined />}
-                        >
-                            <Link to="/ahs-sumber">AHS Sumber</Link>
-                        </Menu.Item>
-                        <Menu.Item key="/hs">
-                            <Link to="/hs">Harga Satuan</Link>
-                        </Menu.Item>
-                        <Menu.Item key="/wilayah">
-                            <Link to="/wilayah">Wilayah</Link>
-                        </Menu.Item>
-                    </SubMenu>
-                    {/* <Menu.Item key="9" icon={<FileOutlined />} /> */}
-                </Menu>
-            </Sider>
-        </>
-    );
+                        </Menu.Item> */}
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Data Source">
+            <Menu.Item
+              key="/ahs-sumber"
+              // icon={<PieChartOutlined />}
+            >
+              <Link to="/ahs-sumber">AHS Sumber</Link>
+            </Menu.Item>
+            <Menu.Item key="/hs">
+              <Link to="/hs">Harga Satuan</Link>
+            </Menu.Item>
+            <Menu.Item key="/wilayah">
+              <Link to="/wilayah">Wilayah</Link>
+            </Menu.Item>
+          </SubMenu>
+          {/* <Menu.Item key="9" icon={<FileOutlined />} /> */}
+        </Menu>
+      </Sider>
+    </>
+  );
 };
 
 export default Sidebar;
